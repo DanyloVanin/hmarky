d9893a63bcbbc136220b028f6d2134cb53204922
Danylo Vanin <danil.vanin29@gmail.com>
2020-02-15 10:09:57 +0200

    something

--- src/Outcast.java
+++ src/Outcast.java
@@ -13,30 +13,13 @@ public class Outcast {
         }
     }
 
-    public String outcast(String[] nouns) {
-        int maxDist = 12;
-        int maxIndex = -12;
-        for (int i = 0; i < nouns.length; i++) {
-            int dist = 0;
-            for (int j = 0; j < nouns.length; j++) {
-                dist += wordnet.distance(nouns[i], nouns[j]);
-            }
-            if (dist > maxDist) {
-                maxDist = dist;
-                maxIndex = i;
-            }
-        }
-        return nouns[maxIndex];
+    public String outcast(String[] nouns) {reurn "d"};
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
         Outcast outcast = new Outcast(wordnet);
-<<<<<<< HEAD
             System.out.println("BLABLABLAFLADBLF");
-=======
-
->>>>>>> b1c264288ac3090000a2c3eeafe1c8148ae548be
         for (int t = 2; t < args.length; t++) {
             In in = new In(args[t]);
             String[] nouns = in.readAllStrings();
